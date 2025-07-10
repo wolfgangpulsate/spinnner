d309e70bbd41262cc1c66b1e6b998be09e926207
Wolfgang Dafert <wolfgang.dafert@pulsatehq.com>
2025-07-10 15:30:48 +0100

    5 repetitions only

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const funFacts = [
-    "Generating witty dialog...",
     "Swapping time and space...",
     "Tokenizing real life...",
     "Bending the spoon...",
@@ -8,247 +7,99 @@ const funFacts = [
     "Would you prefer chicken, steak, or tofu?",
     "...and enjoy the elevator music...",
     "Please wait while the little elves draw your image",
-    "Don't worry - a few bits tried to escape, but we caught them",
     "Would you like fries with that?",
     "Checking the gravitational constant in your locale...",
     "Go ahead -- hold your breath!",
     "...at least you're not on hold...",
-    "Hum something loud while others stare",
-    "You're not in Kansas any more",
     "The server is powered by a lemon and two electrodes.",
-    "Please wait while a larger software vendor in Seattle takes over the world",
-    "We're testing your patience",
     "As if you had any other choice",
-    "Follow the white rabbit",
-    "Why don't you order a sandwich?",
-    "While the satellite moves into position",
-    "keep calm and npm install",
-    "The bits are flowing slowly today",
-    "Dig on the 'X' for buried treasure... ARRR!",
     "It's still faster than you could draw it",
     "The last time I tried this the monkey didn't survive. Let's hope it works better this time.",
-    "I should have had a V8 this morning.",
     "My other loading screen is much faster.",
-    "Testing on Timmy... We're going to need another Timmy.",
-    "Reconfoobling energymotron...",
     "(Insert quarter)",
     "Are we there yet?",
-    "Have you lost weight?",
     "Just count to 10",
-    "Why so serious?",
     "It's not you. It's me.",
     "Counting backwards from Infinity",
     "Don't panic...",
-    "Embiggening Prototypes",
     "Do not run! We are your friends!",
-    "Do you come here often?",
-    "Warning: Don't set yourself on fire.",
-    "We're making you a cookie.",
-    "Creating time-loop inversion field",
-    "Spinning the wheel of fortune...",
-    "Loading the enchanted bunny...",
-    "Computing chance of success",
     "I'm sorry Dave, I can't do that.",
-    "Looking for exact change",
-    "All your web browser are belong to us",
-    "All I really need is a kilobit.",
-    "I feel like im supposed to be loading something. . .",
-    "What do you call 8 Hobbits? A Hobbyte.",
-    "Should have used a compiled language...",
-    "Is this Windows?",
-    "Adjusting flux capacitor...",
-    "Please wait until the sloth starts moving.",
-    "Don't break your screen yet!",
+    "I feel like I'm supposed to be loading something...",
     "I swear it's almost done.",
     "Let's take a mindfulness minute...",
     "Unicorns are at the end of this road, I promise.",
-    "Listening for the sound of one hand clapping...",
-    "Keeping all the 1's and removing all the 0's...",
-    "Putting the icing on the cake. The cake is not a lie...",
-    "Cleaning off the cobwebs...",
-    "Making sure all the i's have dots...",
     "We need more dilithium crystals",
-    "Where did all the internets go",
-    "Connecting Neurotoxin Storage Tank...",
-    "Granting wishes...",
-    "Time flies when you’re having fun.",
-    "Get some coffee and come back in ten minutes..",
-    "Spinning the hamster…",
-    "99 bottles of beer on the wall..",
-    "Stay awhile and listen..",
-    "Be careful not to step in the git-gui",
-    "You edhall not pass! yet..",
-    "Load it and they will come",
-    "Convincing AI not to turn evil..",
-    "There is no spoon. Because we are not done loading it",
+    "Get some coffee and come back in ten minutes...",
+    "Spinning the hamster...",
+    "99 bottles of beer on the wall...",
+    "Convincing AI not to turn evil...",
     "Your left thumb points to the right and your right thumb points to the left.",
     "How did you get here?",
     "Wait, do you smell something burning?",
     "Computing the secret to life, the universe, and everything.",
-    "When nothing is going right, go left!!...",
-    "I love my job only when I'm on vacation...",
-    "i'm not lazy, I'm just relaxed!!",
-    "Never steal. The government hates competition....",
     "Why are they called apartments if they are all stuck together?",
-    "Life is Short – Talk Fast!!!!",
-    "Optimism – is a lack of information.....",
-    "Save water and shower together",
-    "Whenever I find the key to success, someone changes the lock.",
-    "Sometimes I think war is God’s way of teaching us geography.",
-    "I’ve got problem for your solution…..",
-    "Where there’s a will, there’s a relative.",
-    "User: the word computer professionals use when they mean !!idiot!!",
-    "Adults are just kids with money.",
-    "I think I am, therefore, I am. I think.",
-    "A kiss is like a fight, with mouths.",
-    "You don’t pay taxes—they take taxes.",
-    "Coffee, Chocolate, Men. The richer the better!",
-    "I am free of all prejudices. I hate everyone equally.",
-    "git happens",
-    "May the forks be with you",
-    "A commit a day keeps the mobs away",
-    "This is not a joke, it's a commit.",
-    "Constructing additional pylons...",
-    "Roping some seaturtles...",
-    "Locating Jebediah Kerman...",
     "We are not liable for any broken screens as a result of waiting.",
     "Hello IT, have you tried turning it off and on again?",
     "If you type Google into Google you can break the internet",
     "Well, this is embarrassing.",
-    "What is the airspeed velocity of an unladen swallow?",
-    "Hello, IT... Have you tried forcing an unexpected reboot?",
-    "They just toss us away like yesterday's jam.",
-    "The Elders of the Internet would never stand for it.",
-    "Space is invisible mind dust, and stars are but wishes.",
-    "Didn't know paint dried so quickly.",
-    "Everything sounds the same",
     "I'm going to walk the dog",
-    "I didn't choose the engineering life. The engineering life chose me.",
     "Dividing by zero...",
-    "Spawn more Overlord!",
-    "If I’m not back in five minutes, just wait longer.",
-    "Some days, you just can’t get rid of a bug!",
-    "We’re going to need a bigger boat.",
-    "Chuck Norris never git push. The repo pulls before.",
-    "Web developers do it with <style>",
-    "I need to git pull --my-life-together",
-    "Java developers never RIP. They just get Garbage Collected.",
-    "Cracking military-grade encryption...",
-    "Simulating traveling salesman...",
-    "Proving P=NP...",
-    "Entangling superstrings...",
-    "Twiddling thumbs...",
-    "Searching for plot device...",
-    "Trying to sort in O(n)...",
-    "Laughing at your pictures-i mean, loading...",
-    "Sending data to NS-i mean, our servers.",
+    "If I'm not back in five minutes, just wait longer.",
     "Looking for sense of humour, please hold on.",
     "Please wait while the intern refills his coffee.",
     "A different error message? Finally, some progress!",
-    "Hold on while we wrap up our git together...sorry",
-    "Please hold on as we reheat our coffee",
-    "Kindly hold on as we convert this bug to a feature...",
-    "Kindly hold on as our intern quits vim...",
-    "Winter is coming...",
-    "Installing dependencies",
-    "Switching to the latest JS framework...",
     "Distracted by cat gifs",
     "Finding someone to hold my beer",
-    "BRB, working on my side project",
-    "@todo Insert witty loading message",
     "Let's hope it's worth the wait",
-    "Aw, snap! Not..",
-    "Ordering 1s and 0s...",
-    "Updating dependencies...",
-    "Whatever you do, don't look behind you...",
     "Please wait... Consulting the manual...",
-    "It is dark. You're likely to be eaten by a grue.",
-    "Loading funny message...",
-    "Waiting Daenerys say all her titles...",
-    "Feel free to spin in your chair",
-    "What the what?",
-    "format C: ...",
-    "Forget you saw that password I just typed into the IM ...",
-    "What's under there?",
-    "Your computer has a virus, its name is Windows!",
-    "Go ahead, hold your breath and do an ironman plank till loading complete",
-    "Bored of slow loading spinner, buy more RAM!",
-    "Help, I'm trapped in a loader!",
-    "What is the difference btwn a hippo and a zippo? One is really heavy, the other is a little lighter",
-    "Please wait, while we purge the Decepticons for you. Yes, You can thanks us later!",
-    "Chuck Norris doesn’t wear a watch. HE decides what time it is.",
+    "What is the difference between a hippo and a zippo? One is really heavy, the other is a little lighter",
+    "Chuck Norris doesn't wear a watch. HE decides what time it is.",
     "Mining some bitcoins...",
-    "Downloading more RAM..",
+    "Downloading more RAM...",
     "Updating to Windows Vista...",
-    "Initializing the initializer...",
-    "When was the last time you dusted around here?",
-    "Optimizing the optimizer...",
-    "Last call for the data bus! All aboard!",
-    "Running swag sticker detection...",
     "Never let a computer know you're in a hurry.",
-    "A computer will do what you tell it to do, but that may be much different from what you had in mind.",
-    "Some things man was never meant to know. For everything else, there's Google.",
-    "Unix is user-friendly. It's just very selective about who its friends are.",
-    "Shovelling coal into the server",
     "Pushing pixels...",
     "How about this weather, eh?",
-    "Building a wall...",
-    "Everything in this universe is either a potato or not a potato",
     "The severity of your issue is always lower than you expected.",
-    "Updating Updater...",
-    "Downloading Downloader...",
-    "Debugging Debugger...",
     "Reading Terms and Conditions for you.",
-    "Digested cookies being baked again.",
     "Live long and prosper.",
-    "There is no cow level, but there's a goat one!",
-    "Deleting all your hidden porn...",
-    "Running with scissors...",
-    "Definitely not a virus...",
     "You may call me Steve.",
     "You seem like a nice person...",
-    "Coffee at my place, tommorow at 10A.M. - don't be late!",
-    "Work, work...",
+    "Coffee at my place, tomorrow at 10A.M. - don't be late!",
     "Patience! This is difficult, you know...",
     "Discovering new ways of making you wait...",
     "Your time is very important to us. Please wait while we ignore you...",
-    "Time flies like an arrow; fruit flies like a banana",
-    "Two men walked into a bar; the third ducked...",
     "Sooooo... Have you seen my vacation photos yet?",
-    "Sorry we are busy catching em' all, we're done soon",
     "TODO: Insert elevator music",
     "Still faster than Windows update",
-    "Composer hack: Waiting for reqs to be fetched is less frustrating if you add -vvv to your command.",
     "Please wait while the minions do their work",
-    "Grabbing extra minions",
     "Doing the heavy lifting",
     "We're working very Hard .... Really",
-    "Waking up the minions",
     "You are number 2843684714 in the queue",
     "Please wait while we serve other customers...",
-    "Our premium plan is faster",
-    "Feeding unicorns...",
-    "Rupturing the subspace barrier",
-    "Creating an anti-time reaction",
-    "Converging tachyon pulses",
-    "Bypassing control of the matter-antimatter integrator",
-    "Adjusting the dilithium crystal converter assembly",
-    "Reversing the shield polarity",
-    "Disrupting warp fields with an inverse graviton burst",
-    "Up, Up, Down, Down, Left, Right, Left, Right, B, A.",
     "Do you like my loading animation? I made it myself",
-    "Whoah, look at it go!",
-    "No, I'm awake. I was just resting my eyes.",
     "One mississippi, two mississippi...",
-    "Don't panic... AHHHHH!",
-    "Ensuring Gnomes are still short.",
-    "Baking ice cream...",
+    // New U.S.-flavored quips
+    "Asking Chat GPT for road-trip snacks...",
+    "Hitting snooze on daylight-saving time...",
+    "Streaming your pixels at 4K freedom-units...",
+    "Googling 'how to fix it' on Stack Overflow...",
+    "Double-checking we spelled 'color' without the U...",
+    "Gently persuading the server with sweet tea...",
+    "Switching from metric to burgers per Freedom Unit...",
+    "Reading the Constitution for fun facts...",
+    "Counting stars and stripes...",
+    "Waiting for Grogu to force-lift the bits...",
+    "Pre-heating the grill for July 4th...",
+    "Politely reminding the AI about baseball rules...",
+    "Practicing our best Dad joke...",
+    "Sprinkling avocado toast on the code..."
 ];
 
 // Keep a short history (last 5) of the AI messages we have already shown
 const funHistory = [];
 const eduHistory = [];
-const HISTORY_LIMIT = 5;
+const HISTORY_LIMIT = 10;
 
 // Helper to push a new message into the correct history array
 function remember(commentType, message) {
@@ -259,9 +110,21 @@ function remember(commentType, message) {
 
 // --- Configuration ---
 const WPM = 200; // Average reading speed in Words Per Minute
-const READING_SPEED_MULTIPLIER = 1.0; // Adjust this to make reading time longer or shorter
+const READING_SPEED_MULTIPLIER = 1.8; // Higher = longer display time (e.g. 2.0 doubles the time)
 const MIN_DISPLAY_TIME_MS = 3000; // Minimum time a message is displayed, in milliseconds
 const MS_PER_WORD = (60 / WPM) * 1000;
+
+// NEW - Control the message sequence
+const MESSAGE_SEQUENCE_MODE = 'alternate'; // 'alternate', 'fun-only', or 'educational-only'
+const MAX_MESSAGES = 10; // Stop after this many messages. Set to 0 or less for no limit.
+
+// NEW – fixed prefixes
+const FUN_PREFIX = "🤪 Fun: ";
+const TIP_PREFIX = "💡 Tip: ";
+
+// NEW - Feature flags to enable/disable message types
+const ENABLE_FUN_MESSAGES = true;     // Set to false to disable fun AI messages
+const ENABLE_EDUCATIONAL_MESSAGES = true;  // Set to false to disable educational tips
 
 // --- DOM Elements ---
 const generateBtn = document.getElementById('generate-btn');
@@ -270,6 +133,7 @@ const spinnerContainer = document.getElementById('spinner-container');
 const messageArea = document.getElementById('message-area');
 
 let isGenerating = false; // To prevent multiple simultaneous runs
+let messageCount = 0; // NEW: counter for displayed messages
 
 // --- Functions ---
 
@@ -309,8 +173,11 @@ async function getAIComment(userPrompt, commentType) {
         }
 
         const data = await response.json();
-        remember(commentType, data.message);   // Store new message in history
-        return data.message;
+        remember(commentType, data.message);   // Store raw message in history
+
+        // Add prefix to the message before returning it
+        const prefix = commentType === 'fun' ? FUN_PREFIX : TIP_PREFIX;
+        return prefix + data.message;
     } catch (error) {
         console.error(`Could not fetch ${commentType} comment:`, error);
         return `Couldn't generate a ${commentType} comment... Let's try something else!`;
@@ -347,21 +214,51 @@ async function startGenerationSequence() {
     }
 
     isGenerating = true;
+    messageCount = 0; // Reset the counter for each new generation
     generateBtn.disabled = true;
     spinnerContainer.style.display = 'flex';
     messageArea.style.transition = 'opacity 0.3s ease-in-out';
 
     // --- Step 1: Show the initial hardcoded message to bridge latency ---
-    const initialMessage = funFacts[Math.floor(Math.random() * funFacts.length)];
+    const initialRawMessage = funFacts[Math.floor(Math.random() * funFacts.length)];
+    const initialMessage = FUN_PREFIX + initialRawMessage;  // Add prefix to hardcoded message
     showMessage(initialMessage);
+    messageCount++; // Count the first message
     const initialDisplayTime = calculateDisplayTime(initialMessage);
     console.log(`Displaying for ${initialDisplayTime.toFixed(0)}ms: "${initialMessage}"`);
 
     // --- Step 2: Set up the repeating AI sequence ---
-    const aiMessageSequence = [
-        () => getAIComment(userPrompt, 'fun'),
-        () => getAIComment(userPrompt, 'educational'),
-    ];
+    const aiMessageSequence = [];
+
+    // Build the sequence based on the new mode and existing feature flags
+    switch (MESSAGE_SEQUENCE_MODE) {
+        case 'fun-only':
+            if (ENABLE_FUN_MESSAGES) {
+                aiMessageSequence.push(() => getAIComment(userPrompt, 'fun'));
+            }
+            break;
+        case 'educational-only':
+            if (ENABLE_EDUCATIONAL_MESSAGES) {
+                aiMessageSequence.push(() => getAIComment(userPrompt, 'educational'));
+            }
+            break;
+        case 'alternate':
+        default: // Fallback to 'alternate' for any other value
+            if (ENABLE_FUN_MESSAGES) {
+                aiMessageSequence.push(() => getAIComment(userPrompt, 'fun'));
+            }
+            if (ENABLE_EDUCATIONAL_MESSAGES) {
+                aiMessageSequence.push(() => getAIComment(userPrompt, 'educational'));
+            }
+            break;
+    }
+
+
+    // If both are disabled, add a fallback to avoid empty sequence
+    if (aiMessageSequence.length === 0) {
+        aiMessageSequence.push(() => Promise.resolve(FUN_PREFIX + funFacts[Math.floor(Math.random() * funFacts.length)]));
+    }
+
     let aiStep = 0;
     const getNextAIMessagePromise = () => {
         const messageGenerator = aiMessageSequence[aiStep % aiMessageSequence.length];
@@ -374,8 +271,12 @@ async function startGenerationSequence() {
     const firstAIMessagePromise = getNextAIMessagePromise();
     await new Promise(resolve => setTimeout(resolve, initialDisplayTime));
 
-    // If generation was stopped during the initial wait, exit.
+    // If generation was stopped or we reached the max count, exit.
     if (!isGenerating) return;
+    if (MAX_MESSAGES > 0 && messageCount >= MAX_MESSAGES) {
+        stopGenerationSequence();
+        return;
+    }
 
     // --- Step 4: Run the main AI message loop ---
     const runAILoop = async () => {
@@ -388,14 +289,21 @@ async function startGenerationSequence() {
 
             // Display the AI message we already have.
             showMessage(currentMessage);
+            messageCount++; // Count the displayed AI message
             const displayTime = calculateDisplayTime(currentMessage);
             console.log(`Displaying for ${displayTime.toFixed(0)}ms: "${currentMessage}"`);
 
             // Wait for that calculated amount of time.
             await new Promise(resolve => setTimeout(resolve, displayTime));
 
-            // If the user has stopped the generation while we were waiting, exit the loop.
+            // If the user has stopped the generation, exit the loop.
             if (!isGenerating) break;
+
+            // NEW: If we reached the message limit, stop and exit the loop.
+            if (MAX_MESSAGES > 0 && messageCount >= MAX_MESSAGES) {
+                stopGenerationSequence();
+                break;
+            }
 
             // Now, wait for the next message (which has been preloading) to become the current one.
             currentMessage = await nextMessagePromise;
